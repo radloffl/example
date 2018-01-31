96dfd21b0fe1f221fc1c75a9ed4cca69bbac51ae
Logan Radloff <logan.radloff@banno.com>
2018-01-31 13:34:33 -0600

    type param cyclic reference example

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,12 @@
 package example
 
-object Hello extends Greeting with App {
-  println(greeting)
-}
+import eu.timepit.refined._
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.string._
+import scalaz._
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+package object model {
+  type TIN = String Refined TINREGEX
+  type TINREGEX = MatchesRegex[W.`"[0-9]{9}"`.T]
+  implicit val TINShow: Show[TIN] = Show.shows[TIN](_ => "MASKED_TIN")
 }
